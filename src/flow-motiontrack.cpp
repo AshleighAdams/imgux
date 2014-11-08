c4db89aacc4de9b4cde9755006e7fb05bc5b13aa
Kate Adams <frozenfish51@gmail.com>
2014-11-08 17:49:44 +0000

    Fixed motion tracking.

--- src/flow-motiontrack.cpp
+++ src/flow-motiontrack.cpp
@@ -78,8 +78,8 @@ struct Tracked
 		{
 			if(first)
 			{
-				sx = sy = island->x;
-				ex = ey = island->y;
+				sx = ex = island->x;
+				sy = ey = island->y;
 				first = false;
 			}
 			
@@ -98,7 +98,7 @@ struct Tracked
 		this->w = ex - sx;
 		this->h = ey - sy;
 		
-		if(this->lifetime != 0)
+		if(this->lifetime > 1)
 		{
 			// calculate avg vel
 			this->_center_history.push_back(std::tuple<double,double>{(this->cx() - scx) / delta, (this->cy() - scy) / delta});
